ed27507b38f53fb789a4bd70214ce4fae0104d7a
TABISH <tabishjaved@hotmail.com>
2023-02-19 00:01:55 +0530

    added styles files

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.set("view engine", "ejs")
 // setup second app option to define path where views folder exists
 app.set("views", path.join(__dirname, "views"))
 
+//=== SETUP MIDDLEWARE FOR STATIC CONTENT ===|
+app.use(express.static("public"))
+
 app.use(authRoutes)
 
 app.listen(3000)
