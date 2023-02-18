8690f03dbe2e7eb4ad1a0affc82fa4cfe378d7f0
TABISH <tabishjaved@hotmail.com>
2023-02-18 23:02:29 +0530

    added some ejs views and includes

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+// import inbuilt modules
+const path = require("path")
+
 // import third party modules
 const express = require("express")
 
 // import code outsourced files
 const authRoutes = require("./routes/auth.routes")
 
+// setup app object by calling express function
 const app = express()
+
+//=== SETUP EJS VIEW ENGINE ===|
+// setup app option for rendering views using "ejs" module
+app.set("view engine", "ejs")
+// setup second app option to define path where views folder exists
+app.set("views", path.join(__dirname, "views"))
 
 app.use(authRoutes)
 
