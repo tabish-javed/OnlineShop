378af260e5e43fceaaa3750f7e95f4b70c7ae892
TABISH <tabishjaved@hotmail.com>
2023-03-04 13:57:10 +0530

    added image upload feature

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,9 +2,12 @@
 const router = require("express").Router()
 
 const adminController = require("../controllers/admin.controller")
+const imageUploadMiddleware = require("../middleware/image-upload")
 
 router.get("/products", adminController.getProducts)
 
 router.get("/products/new", adminController.getNewProduct)
+
+router.post("/products", imageUploadMiddleware, adminController.createNewProduct)
 
 module.exports = router
